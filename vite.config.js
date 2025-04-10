2dbb3531f0c301deb7ea95501d7be98ecda448a1
HossamAlsaedi <hossam_ksa@hotmail.com>
2025-04-10 15:14:57 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/QnA_Game/', // This matches the path in the "homepage" field of package.json
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/QnA_Game/' : '/',
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+  }
 })
