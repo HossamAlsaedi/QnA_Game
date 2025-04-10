1b995fb0dc523cc72c514b2234b08cd8b6a05a77
HossamAlsaedi <hossam_ksa@hotmail.com>
2025-04-10 14:52:15 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: 'QnA_Game',
   plugins: [
     vue(),
     vueDevTools(),
