0540c877b71e68da1505f91c4945b8ba0e9a2551
HossamAlsaedi <hossam_ksa@hotmail.com>
2025-04-10 14:58:39 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'QnA_Game',
+  base: '/QnA_Game/',  // Make sure it has leading and trailing slashes
   plugins: [
     vue(),
     vueDevTools(),
